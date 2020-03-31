2b49bde19a30e5e6263f56f1876df98394e2e88f
Loreto Barroso <60405184+loretobarroso322@users.noreply.github.com>
2020-03-31 11:51:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -126,29 +126,18 @@ for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
 
     //add in the active class
+    this.classList.toggle("active");
 
     //select sibling element
-
+    let panel = this.nextElementSibling;
     //if sibling is open, close it, if closed, open it
     if (panel.style.maxHeight){
       //panel is open
 
     } else {
       //panel is closed
-
+      panel.style.maxHeight = panel.scrollHeight + "px"; //add this line
+    }
     }
   });
-}
-//add in the active class
-this.classList.toggle("active");
-//select sibling element
-let panel = this.nextElementSibling;
-//if sibling is open, close it, if closed, open it
-if (panel.style.maxHeight){
-  //panel is open open
-  panel.style.maxHeight = null; //add this line
-
-} else {
-  //panel is closed
-  panel.style.maxHeight = panel.scrollHeight + "px"; //add this line
 }
